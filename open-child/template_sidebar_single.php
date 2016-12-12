4633f501aaa35a1e6b6d423180153c7f455dbe1e
eddt <etwilbeck@marketingincolor.com>
2016-12-12 16:05:52 -0600

    added missing save list for sidebar

--- open-child/template_sidebar_single.php
+++ open-child/template_sidebar_single.php
@@ -23,9 +23,9 @@
 <div class="widget mkd-rpc-holder">
     <div class="widget widget_categories">
         <div class="mkd-rpc-content">             
-            <!--- Insert Ads here --->
-            <?php if (function_exists('drawAdsPlace')) drawAdsPlace(array('id' => 1), true); ?>
-            <!--- Ads end here --->            
+            <!-- Insert Ads here -->
+            <?php //if (function_exists('drawAdsPlace')) drawAdsPlace(array('id' => 1), true); ?>
+            <!-- Ads end here -->            
         </div>
     </div>
 </div>
