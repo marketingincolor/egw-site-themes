cd6bbb4a5139c458ab071573e0661311f0b0bdb3
eddt <etwilbeck@marketingincolor.com>
2016-12-15 13:09:35 -0600

    fixes to video page

--- open-child/page-videos.php
+++ open-child/page-videos.php
@@ -6,14 +6,13 @@
  * Date - 21-06-2016
  * For displaying featured article and videos blog
  */
-?>
-
-<?php
-get_header();
+$member_location = get_egw_member_location();
+$tag_not_in = egw_tag_not_in($member_location);
 $category = '';
 list($post_per_section, $post_type) = scroll_loadpost_settings();
 $post_type=array('videos');
 ?>
+<?php get_header(); ?>
 <div class="mkd-content">
     <div class="mkd-content-inner">         
         <div class="mkd-full-width">
@@ -26,18 +25,53 @@ $post_type=array('videos');
               videos section
              */
             ?>
+
+            <?php if (current_user_can('access_village_content' )): ?>
+
+            <div class="vc_empty_space" style="height: 0px"><span class="vc_empty_space_inner"></span></div>
+            <div class="mkd-container">
+                <div class="mkd-container-inner clearfix">
+                    <div class="mkd-two-columns-75-25  mkd-content-has-sidebar clearfix">
+                        <div class="mkd-column1 mkd-content-left-from-sidebar">
+                            <div class="mkd-column-inner">
+                                <?php
+                                $my_query = null;
+                                $my_query = discussion_custom_category_query('videos', $category, $post_per_section, $tag_not_in);
+                                global $wp_query;
+                                get_template_part('block/category-blog-list');   
+                                ?>
+                            </div>
+                        </div>      
+                        <div class="mkd-column2">
+                            <div class="mkd-column-inner">
+                                <aside class="mkd-sidebar" style="transform: translateY(0px);">
+                                    <div class="widget widget_apsc_widget"> 
+                                     <div class="vc_empty_space" style="height: 40px"><span class="vc_empty_space_inner"></span></div>     
+                                        <?php get_template_part('sidebar/template-sidebar-home'); ?>
+                                    </div>    
+                                </aside>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- User Not Signed In -->
+            <?php else: ?>
+
             <div style="" class="vc_row wpb_row vc_row-fluid mkd-section mkd-content-aligment-left mkd-grid-section">
                 <div class="mkd-container-inner clearfix">
                     <div class="mkd-section-inner-margin clearfix">
                         <?php
                         $my_query = null;
-                        $my_query = discussion_custom_category_query('videos', $category, $post_per_section);
+                        $my_query = discussion_custom_category_query('videos', $category, $post_per_section, $tag_not_in);
                         global $wp_query;
                        get_template_part('block/category-blog-list');   
                         ?>
                     </div>
                 </div><!-- #content -->
             </div>
+
+            <?php endif; ?>
         </div>
     </div>
 </div>
