ac39ce01d5c19e2ea3dc4eb5ff9e3d1c8ed42f69
Adam Doe <adoe@marketingincolor.com>
2017-03-06 17:08:34 -0500

    Added Stop to fix Bug

--- open-child/assets/js/tooltip-animations.js
+++ open-child/assets/js/tooltip-animations.js
@@ -6,11 +6,11 @@
 
 jQuery(document).ready(function(){
 	jQuery('.sponsored-post-bar').mouseenter(function(){
-		jQuery(this).animate({height: '100%', opacity: '0.95'}, 300);
+		jQuery(this).stop().animate({height: '100%', opacity: '0.95'}, "slow");
 		jQuery('.tooltip-text').css('display', 'block');
 	});
 	jQuery('.sponsored-post-bar').mouseleave(function(){
-		jQuery(this).animate({height: '25px', opacity: '1'}, 300);
+		jQuery(this).stop().animate({height: '25px', opacity: '1'}, "slow");
 		jQuery('.tooltip-text').hide();
 	});
 	// jQuery('.sponsored-by').mouseenter(function(){
