696e822c355faee5f148e617738176105268f9ba
eddt <etwilbeck@marketingincolor.com>
2017-03-13 16:05:40 -0500

    footer fixes

--- open-child/block/template-foot-newsletter-form.php
+++ open-child/block/template-foot-newsletter-form.php
@@ -1,6 +1,6 @@
 <?php if (current_user_can(ACCESS_VILLAGE_CONTENT)): ?>
 <?php else: ?>
-<div class="widget mkd-rpc-holder">
+<div class="widget mkd-rpc-holder foot-cta">
     <div class="news-field-row clearfix egw-show-for-large-up" id="form-container-foot">
         <h3 class="news-field-cta-title">Get FREE Wellness Tips Delivered!</h3>
         <div class="news-field-cta-form">
@@ -9,8 +9,8 @@
                 <input type="hidden" name="form_title" value="Newsletter CTA"/>
                 <div class="form-control-wrap your-email"><input type="email" id="foot-your-email" name="your-email" placeholder=" EMAIL ADDRESS" value="" size="40" /></div>
                 <div class="form-control-wrap your-zip"><input type="text" id="foot-your-zip" name="your-zip" placeholder=" ZIP CODE" value="" size="40" /></div>
-                <div class="form-control-wrap your-terms"><input type="checkbox" checked value="terms" id="news-foot-terms" class="form-control terms" />I accept your <a href="https://myevergreenwellness.com/terms-and-conditions/" target="_blank">Terms &amp; Conditions</a></div>
                 <div class="form-control-wrap foot-submit"><input type="submit" value="Sign Me Up!" id="news-foot-submit" class="form-control submit" /></div>
+                <div class="form-control-wrap your-terms"><input type="checkbox" checked value="terms" id="news-foot-terms" class="form-control terms" />I accept your <a href="https://myevergreenwellness.com/terms-and-conditions/" target="_blank">Terms &amp; Conditions</a></div>
                 <?php //do_shortcode('[ssnfinclude placement="foot"]'); ?>
             </form>
         </div>
@@ -18,7 +18,7 @@
 </div>
 <script type='text/javascript'>
     jQuery(document).ready(function($) {
-        var message = '<h3>Welcome!</h3><h4>Please check your email* for more information. We hope you enjoy Evergreen Wellness.</h4><h5>*If you don\'t see an email from us, please check your spam folder.</h5>';
+        var message = '<h3>Welcome!</h3><h4>Please check your email* for more information.<br />We hope you enjoy Evergreen Wellness.</h4><h5>*If you don\'t see an email from us, please check your spam folder.</h5>';
         $('#news-foot-submit').click(function() {
             var email = $("input#foot-your-email").val();
             var zip = $("input#foot-your-zip").val();
