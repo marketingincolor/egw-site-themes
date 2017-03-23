2f530fd65bed324869182abf19e7396bc7d39f62
eddt <etwilbeck@marketingincolor.com>
2017-03-23 09:06:01 -0500

    layout spacing adjustments

--- open-child/category.php
+++ open-child/category.php
@@ -32,7 +32,7 @@ list($post_per_section,$post_type)=scroll_loadpost_settings();
                         <div class="mkd-two-columns-75-25  mkd-content-has-sidebar clearfix">
                             <div class="mkd-column1 mkd-content-left-from-sidebar">
                                 <div class="mkd-column-inner">
-                                    <div class="vc_empty_space" style="height: 20px"><span class="vc_empty_space_inner"></span></div> 
+                                    <div class="vc_empty_space" style="height: 0px"><span class="vc_empty_space_inner"></span></div> 
                                    <?php
                                     $my_query = null;
                                     $my_query = discussion_custom_categorylist_query($post_type,$category_id,$post_per_section,$tag_not_in);   
@@ -47,7 +47,7 @@ list($post_per_section,$post_type)=scroll_loadpost_settings();
                                 <div class="mkd-column-inner">
                                     <aside class="mkd-sidebar" style="transform: translateY(0px);">
                                         <div class="widget widget_apsc_widget">  
-                                            <div class="vc_empty_space" style="height: 20px"><span class="vc_empty_space_inner"></span></div> 
+                                            <div class="vc_empty_space" style="height: 40px"><span class="vc_empty_space_inner"></span></div> 
                                            <?php get_template_part('sidebar/template-sidebar-home'); ?>
                                         </div>    
                                     </aside>
