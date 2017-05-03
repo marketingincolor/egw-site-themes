cbdb3926b6e800d6dceba33dfd8fa9021b089365
Adam Doe <adoe@marketingincolor.com>
2017-05-03 15:31:55 -0400

    Adds ability to turn off comments at the post level

--- open-child/single-sponsored_posts.php
+++ open-child/single-sponsored_posts.php
@@ -139,14 +139,22 @@
                     <?php echo do_shortcode('[AuthorRecommendedPosts]'); ?>
                 </div>
 
-                <!-- Show Facebook Comments or WP Comments -->
-                <?php if( get_option('egw_fb_comments_single_sponsored_posts') && get_option('egw_fb_comments_api_key' ) ): ?>
-                <div class="fb-comments" data-href="<?php the_permalink();?>" data-numposts="10" data-width="100%" data-colorscheme="light"></div>
-                <?php else:
-                    get_template_part('block/comments-guidelines');  
-                    comments_template('', true);
-                endif; ?>
-                <!-- /Show Facebook Comments or WP Comments -->
+                <!-- Check for comments open AND Show Facebook Comments or WP Comments -->
+                <?php 
+                if ( ! comments_open() ) { ?>
+                    <!-- If comments are closed display nothing. -->
+                    <p></p>
+                <?php }
+                else {
+                    if( get_option('egw_fb_comments_single_posts') && get_option('egw_fb_comments_api_key' ) ):
+                        get_template_part('block/comments-guidelines'); ?>
+                        <div class="fb-comments" data-href="<?php the_permalink();?>" data-numposts="10" data-width="100%" data-colorscheme="light"></div>
+                    <?php else:
+                        get_template_part('block/comments-guidelines');
+                        comments_template('', true);
+                    endif; 
+                } ?>
+                <!-- /Check for comments open AND Show Facebook Comments or WP Comments -->
 
                 </div>
             </div>
